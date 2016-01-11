dcd28af41c07d58b5790cb0a621b1df8c1896dbe
Jean Droguett <jdroguett@gmail.com>
2016-01-11 11:50:54 -0300

    follower methods

--- lib/pipedrive/person.rb
+++ lib/pipedrive/person.rb
@@ -6,18 +6,7 @@ module Pipedrive
       def find_or_create_by_name(name, opts={})
         find_by_name(name, :org_id => opts[:org_id]).first || create(opts.merge(:name => name))
       end
-
-      def add_follower(person_id, user_id)
-        opts = {user_id: user_id}
-        res = post "#{resource_path}/#{person_id}/followers", :body => opts
-        res.success? ? res['data']['id'] : bad_response(res,opts)
-      end
-
-      def del_follower(person_id, follower_id)
-        opts = {user_id: user_id}
-        res = delete "#{resource_path}/#{person_id}/followers/#{user_id}", :body => opts
-        res.success? ? res['data']['id'] : bad_response(res,opts)
-      end
+      
     end
 
     def deals()
@@ -38,7 +27,13 @@ module Pipedrive
       res.success? ? res['data']['id'] : bad_response(res,opts)
     end
 
-     def add_product(opts = {})
+    def del_follower(follower_id)
+      opts = {follower_id: follower_id}
+      res = delete "#{resource_path}/#{id}/followers/#{follower_id}", :body => opts
+      res.success? ? res['data']['id'] : bad_response(res,opts)
+    end
+
+    def add_product(opts = {})
       res = post "#{resource_path}/#{id}/products", :body => opts
       res.success? ? res['data']['product_attachment_id'] : bad_response(res,opts)
     end
